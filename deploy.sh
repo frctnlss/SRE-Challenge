0a78e42914faa4d68eeb13ce2115a8d89e3b22f2
frctnlss <92shea@gmail.com>
2021-02-01 10:10:35 -0700

    update deployment items to remove domain resources. fix formatting of buildspec

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-read -rp "What aws cli profile are you using" Profile
-read -rp "What is the domain you are deploying to" HostedZoneDomain
+read -rp "What aws cli profile are you using: " Profile
+#read -rp "What is the domain you are deploying to: " HostedZoneDomain
 
 sam deploy \
   --no-fail-on-empty-changeset \
   --stack-name "SRE-Challenge" \
-  --template cfn-codepipeline.yml \
+  --template cloudformation.yml \
   --capabilities 'CAPABILITY_NAMED_IAM' \
   --region "us-east-1" \
-  --parameter-overrides \
-    ParameterKey=HostedZoneDomain,ParameterValue="${HostedZoneDomain}" \
-  --Profile "${Profile}"
+  --profile "${Profile}"
+#  --parameter-overrides \
+#    ParameterKey=HostedZoneDomain,ParameterValue="${HostedZoneDomain}" \
